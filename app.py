391eb56426c43f5a614e39c5ff48c00f65f634ff
Ahmad Raza <114290929+moonnight90@users.noreply.github.com>
2023-02-19 21:24:23 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify,request,render_template
 from datetime import datetime
+import base64
 import threading,time
 import json
 import pytz
@@ -54,6 +55,22 @@ def backgroundFun():
     obj.a+=1
     obj.change_date(obj.today_prayer_time())
 
+def base64_encode(data):
+    encoded_bytes = base64.b64encode(data.encode('utf-8'))
+    return encoded_bytes.decode('utf-8')
+
+def base64_decode(encoded_data):
+    decoded_bytes = base64.b64decode(encoded_data.encode('utf-8'))
+    return decoded_bytes.decode('utf-8')
+def check_pass(password:str):
+    c = None
+    with open('credential') as f:
+        j = json.loads(f.read())
+        if base64_encode(password) == j['pass']:
+            c = True
+        else:
+            c = False
+    return c
 
 @app.route('/')
 def index():
@@ -88,10 +105,61 @@ def start():
         else:
             return render_template('startdate.html')
 
+@app.route('/update',methods=['GET','POST'])
+def update():
+    if not started:
+        return jsonify({"Code":"Success","msg":"Calander not started yet"})
+    if request.method == "GET":
+        return render_template('update.html',date = {"d":obj.sd,'m':obj.sm,'y':obj.sy})
+    if request.method == "POST":
+        try:
+            password = request.form.get('pass')
+            if not check_pass(password):
+                return jsonify({"Code":"Not Updated","msg":"Invalid Password"})
+            sd = int(request.form.get("day"))
+            sm = int(request.form.get('mon'))
+            sy = int(request.form.get('year'))
+            if not sd or not sm or not sy:
+                return jsonify({"Code":"Not Updated","msg":"Invalid Data"})
+            obj.sd = sd
+            obj.sm = sm
+            obj.sy = sy            
+            return jsonify({"Code":"Success","msg":"Updated"})
+        except: return jsonify({"Code":"Err","msg":"Invalid Data"})
+@app.route('/admin',methods=["GET"])
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/changepassword',methods=["GET","POST"])
+def changePassword():
+    if request.method == "POST":
+        current = request.form.get('current')
+        if not check_pass(current):
+            return jsonify({"Code":"Not Changed","msg":"Invalid Current Password"})
+        else:
+            npass = request.form.get('new')
+            cpass = request.form.get('confirm')
+            if npass != cpass:
+                return jsonify({"Code":"Not Changed","msg":"New Passwords not matched"})
+            elif len(npass)<6:
+                return jsonify({"Code":"Not Changed","msg":"Enter password of atleast 6 digits"})
+            else:
+                with open('credential','w') as f:
+                    f.write(json.dumps({"pass":base64_encode(npass)}))
+                return jsonify({"Code":"Successfully Changed","msg":""})
+
+    else:
+        return render_template('changepass.html')
+
+
 @app.route('/set',methods=['GET','POST'])
 def set():
     if started:
         if request.method == "POST":
+            password = request.form.get('pass')
+            if not check_pass(password):
+                return jsonify({"Code":"Not Set","msg":"Invalid Password"})
             if request.form.get('check')=="on":
                 obj.big = True
             else:
